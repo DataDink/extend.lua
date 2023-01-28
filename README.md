1771112ecd04c1f2d61564b70cb274aa76377a12
Mark Greenwald <Mark@Greenwald>
2023-01-28 10:56:16 -0800

    Lib & Docs

--- README.md
+++ README.md
@@ -1,2 +1,134 @@
 # templates.lua
-Simplistic object templating and inheritance for lua
+Simplistic object templating and inheritance
+
+## Usage
+>
+> ### Extending A Table
+> *extending the standard table library*
+> ```lua
+> -- Create an extension library
+> local tableEx = extend(table);
+> print(tableEx.getn == table.getn); -- true
+>
+> -- Override a function on the extension library
+> tableEx.getn = function() end;
+> print(tableEx.getn == table.getn); -- false
+>
+> -- Add a new function to the standard library
+> table.newFunc = function() end;
+> print(tableEx.newFunc == table.newFunc); -- true
+>
+> -- Add a new function to the extension library
+> tableEx.extendedFunc = function() end;
+> print(table.extendedFunc == nil); -- true
+> ```
+>
+> ### Creating A Class
+> *extending with a class template*
+> ```lua
+> -- extend(base, template)
+> local MyClass = extend({}, {
+>   stringProperty = "foo",              -- Instance Property
+>   numberProperty = 42,                 -- Instance Property
+>   booleanProperty = true,              -- Instance Property
+>   functionProperty = function() end,   -- Static Property
+>   tableProperty = {},                  -- Static Property
+>   __propertyWithPrefix = 123,          -- Metatable Property
+>   initialize = function(self,...)      -- Constructor
+>     self.constructorArgs = {...};
+>   end,
+> });
+>
+> -- The class will expose static properties
+> print(type(MyClass.stringProperty)); -- nil
+> print(type(MyClass.numberProperty)); -- nil
+> print(type(MyClass.booleanProperty)); -- nil
+> print(type(MyClass.functionProperty)); -- function
+> print(type(MyClass.tableProperty)); -- table
+> print(type(MyClass.__propertyWithPrefix)); -- nil
+>
+> -- The instance will recieve a copy of instance properties
+> local instance = MyClass("a", "b", "c");
+> print(instance.stringProperty); -- "foo"
+> print(instance.numberProperty); -- 42
+> print(instance.booleanProperty); -- true
+> print(type(instance.functionProperty)); -- function
+> print(type(instance.tableProperty)); -- table
+> print(getmetatable(instance).__propertyWithPrefix); -- 123
+> print(instance.constructorArgs[1]); -- a
+> print(instance.constructorArgs[2]); -- b
+> print(instance.constructorArgs[3]); -- c
+> ```
+>
+> ### Inheriting A Table Or Class
+> *a class that inherits the standard table library*
+> ```lua
+> local Element = extend(table, {
+>   initialize = function(self, top, left, right, bottom)
+>     self.top = top;
+>     self.left = top;
+>     self.right = top;
+>     self.bottom = top;
+>   end
+>   validate = function(self)
+>     if (top>bottom or left>right) return false; end
+>     for _,child in ipairs(self) do
+>       if (!child:validate()) then return false; end
+>     end
+>     return true;
+>   end,
+> });
+>
+> local root = Element(5,5,10,10);
+> root:insert(Element(3,7,9,14));
+> root:insert(Element(4,4,8,8));
+> print(root:validate()); -- true
+> root:insert(Element(8,8,4,4));
+> print(root:validate()); -- false
+> ```
+>
+> ### Determining Inheritance
+> *type matching*
+> ```lua
+> local SuperClass = extend(table, {});
+> local SubClass = extend(SuperClass, {});
+> local OtherClass = extend({}, {});
+> local instance = SubClass();
+>
+> print(SubClass:ancestors(instance)); -- true
+> print(SuperClass:ancestors(instance)); -- true
+> print(extend.ancestors(table, instance)); -- true
+> print(OtherClass:ancestors(instance)); -- false
+> ```
+
+## API
+>
+> ### Syntax
+> `(table)prototype extend((table)base, (table)template)`
+> <table>
+>   <tr><th>base</th><td>table</td><td>
+>     The table being extended.<br />
+>     This can be:
+>     <ul>
+>       <li>Any table</li>
+>       <li>Another prototype/class</li>
+>       <li>A standard lua library (e.g. string or table)</li>
+>     </ul>
+>   </td></tr>
+>   <tr><th>template</th><td>table</td><td>
+>     The template for the new prototype/class.<br />
+>     The key names and value types of the template determine how they are handled:
+>     <table>
+>       <tr><th>metavalue</th><td>A key prefixed with "__" will be set to an instances metatable</td></tr>
+>       <tr><th>constructor</th><td>A key named "initialize" with a function value will be treated as a constructor</td></tr>
+>       <tr><th>instance</th><td>String, number and boolean values will be treated as instance values</td></tr>
+>       <tr><th>static</th><td>Function and table values will be treated as static values exposed on both the prototype and the instance</td></tr>
+>     </table>
+>   </td></tr>
+>   <tr><th>prototype</th><td>prototype</td><td>
+>     Calling the extend library returns a prototype (aka class).<br />
+>     The prototype will expose any values from the base and only
+>     static methods from the template.<br /> 
+>     Calling the prototype will return a new instance.
+>   </td></tr>
+> </table>
---
+++ tests.lua
@@ -0,0 +1,522 @@
+local unit = require("luaunit");
+local extend = require("extend");
+
+function testAssignPrefixed()
+  local instance, static, meta, value = {}, {}, {}, "bar";
+  extend.assign(instance, static, meta, "__foo", value);
+  unit.assertEquals(meta.__foo, value);
+  unit.assertIsNil(instance.__foo);
+  unit.assertIsNil(static.__foo);
+end
+
+function testAssignInitialize()
+  local instance, static, meta, value = {}, {}, {}, function() end;
+  extend.assign(instance, static, meta, "initialize", value);
+  unit.assertEquals(meta.initialize, value);
+  unit.assertIsNil(instance.initialize);
+  unit.assertIsNil(static.initialize);
+end
+
+function testAssignFunction()
+  local instance, static, meta, value = {}, {}, {}, function() end;
+  extend.assign(instance, static, meta, "foo", value);
+  unit.assertIsNil(meta.foo);
+  unit.assertIsNil(instance.foo);
+  unit.assertEquals(static.foo, value);
+end
+
+function testAssignTable()
+  local instance, static, meta, value = {}, {}, {}, {};
+  extend.assign(instance, static, meta, "foo", value);
+  unit.assertIsNil(meta.foo);
+  unit.assertIsNil(instance.foo);
+  unit.assertEquals(static.foo, value);
+end
+
+function testAssignString()
+  local instance, static, meta, value = {}, {}, {}, "bar";
+  extend.assign(instance, static, meta, "foo", value);
+  unit.assertIsNil(meta.foo);
+  unit.assertEquals(instance.foo, value);
+  unit.assertIsNil(static.foo);
+end
+
+function testAssignNumber()
+  local instance, static, meta, value = {}, {}, {}, 42;
+  extend.assign(instance, static, meta, "foo", value);
+  unit.assertIsNil(meta.foo);
+  unit.assertEquals(instance.foo, value);
+  unit.assertIsNil(static.foo);
+end
+
+function testAssignBoolean()
+  local instance, static, meta, value = {}, {}, {}, true;
+  extend.assign(instance, static, meta, "foo", value);
+  unit.assertIsNil(meta.foo);
+  unit.assertEquals(instance.foo, value);
+  unit.assertIsNil(static.foo);
+end
+
+function testAssignNil()
+  local instance, static, meta, value = {}, {}, {}, nil;
+  extend.assign(instance, static, meta, "foo", value);
+  unit.assertIsNil(meta.foo);
+  unit.assertIsNil(instance.foo);
+  unit.assertIsNil(static.foo);
+end
+
+function testAncestorsNoArgs() 
+  unit.assertIsFalse(extend.ancestors()); 
+end
+
+function testAncestorsNilInstance() 
+  unit.assertIsFalse(extend.ancestors({}, nil)); 
+end
+
+function testAncestorsNilPrototype() 
+  unit.assertIsFalse(extend.ancestors(nil, nil)); 
+end
+
+function testAncestorsStringInstance() 
+  unit.assertIsFalse(extend.ancestors({}, "foo")); 
+end
+
+function testAncestorsStringPrototype() 
+  local value="foo";
+  unit.assertIsTrue(extend.ancestors(value, value)); 
+end
+
+function testAncestorsNumberInstance() 
+  unit.assertIsFalse(extend.ancestors({}, 42)); 
+end
+
+function testAncestorsNumberPrototype() 
+  local value=42;
+  unit.assertIsFalse(extend.ancestors(value, value)); 
+end
+
+function testAncestorsBooleanInstance() 
+  unit.assertIsFalse(extend.ancestors({}, true)); 
+end
+
+function testAncestorsBooleanPrototype() 
+  local value=true;
+  unit.assertIsFalse(extend.ancestors(value, value)); 
+end
+
+function testAncestorsTables() 
+  local value={};
+  unit.assertIsFalse(extend.ancestors(value, value)); 
+end
+
+function testAncestorsNoMeta() 
+  local index={};
+  local proto=setmetatable({}, {__index=index});
+  unit.assertIsFalse(extend.ancestors(proto, {})); 
+end
+
+function testAncestorsNoIndex() 
+  local index={};
+  local proto=setmetatable({}, {__index=index});
+  unit.assertIsFalse(extend.ancestors(proto, setmetatable({}, {}))); 
+end
+
+function testAncestorsNoMatch() 
+  local index={};
+  local proto=setmetatable({}, {__index=index});
+  unit.assertIsFalse(extend.ancestors(proto, setmetatable({}, {__index={}}))); 
+end
+
+function testAncestorsNoMatchNested() 
+  local index={};
+  local proto=setmetatable({}, {__index=index});
+  unit.assertIsFalse(extend.ancestors(proto, setmetatable({}, {__index=setmetatable({}, {__index={}})}))); 
+end
+
+function testAncestorsMatchZeroDeep() 
+  local index={};
+  local proto=setmetatable({}, {__index=index});
+  unit.assertIsFalse(extend.ancestors(proto, index)); 
+end
+
+function testAncestorsMatchOneDeep() 
+  local index={};
+  local proto=setmetatable({}, {__index=index});
+  unit.assertIsTrue(extend.ancestors(proto, setmetatable({}, {__index=index}))); 
+end
+
+function testAncestorsMatchTwoDeep() 
+  local index={};
+  local proto=setmetatable({}, {__index=index});
+  unit.assertIsTrue(extend.ancestors(proto, setmetatable({}, {__index=setmetatable({}, {__index=index})}))); 
+end
+
+function testExtendNoArgs() unit.assertError(function() extend(); end); end
+
+function testExtendStringBase() unit.assertError(function() extend("foo"); end); end
+
+function testExtendNumberBase() unit.assertError(function() extend(42); end); end
+
+function testExtendFunctionBase() unit.assertError(function() extend(function() end); end); end
+
+function testExtendBooleanBase() unit.assertError(function() extend(true); end); end
+
+function testExtendOnlyBase() unit.assertEquals(type(extend({})), "table"); end
+
+function testExtendReturnsUniquePrototype()
+  local base = {};
+  local prototype = extend(base);
+  unit.assertNotIs(prototype, extend(base));
+end
+
+function testExtendReturnsUniqueConfig()
+  local base = {};
+  local config = getmetatable(extend(base));
+  unit.assertNotIs(config, getmetatable(extend(base)));
+end
+
+function testExtendReturnsUniqueIndex()
+  local base = {};
+  local index = getmetatable(extend(base)).__index;
+  unit.assertNotIs(index, getmetatable(extend(base)).__index);
+end
+
+function testExtendReturnsUniqueInstance()
+  local base = {};
+  local instance = getmetatable(extend(base)).instance;
+  unit.assertNotIs(instance, getmetatable(extend(base)).instance);
+end
+
+function testExtendReturnsUniqueMeta()
+  local base = {};
+  local meta = getmetatable(extend(base)).meta;
+  unit.assertNotIs(meta, getmetatable(extend(base)).meta);
+end
+
+function testExtendSetsCallToInitialize()
+  local base = {};
+  local prototype = extend(base);
+  local config = getmetatable(prototype);
+  unit.assertEquals(config.__call, extend.initialize);
+end
+
+function testExtendIndexNotBase()
+  local base = {};
+  local prototype = extend(base);
+  local config = getmetatable(prototype);
+  unit.assertNotIs(config, base);
+  unit.assertNotIs(config.__index, base);
+end
+
+function testExtendIndexInheritsBase()
+  local base = {};
+  local prototype = extend(base);
+  local config = getmetatable(prototype);
+  unit.assertNotIs(getmetatable(config.__index), base);
+  unit.assertIs(getmetatable(config.__index).__index, base);
+end
+
+function testExtendPrefixedValue()
+  local base = {};
+  local prototype = extend(base, {__foo="bar"});
+  local config = getmetatable(prototype);
+  unit.assertIsNil(base.__foo);
+  unit.assertEquals(config.meta.__foo, "bar");
+end
+
+function testExtendInitializeFunction()
+  local base = {}; local value = function() end;
+  local prototype = extend(base, {initialize=value});
+  local config = getmetatable(prototype);
+  unit.assertIsNil(base.initialize);
+  unit.assertEquals(config.meta.initialize, value);
+end
+
+function testExtendFunction()
+  local base = {}; local value = function() end;
+  local prototype = extend(base, {foo=value});
+  local config = getmetatable(prototype);
+  unit.assertIsNil(base.foo);
+  unit.assertEquals(rawget(config.__index, "foo"), value);
+end
+
+function testExtendTable()
+  local base = {}; local value = {};
+  local prototype = extend(base, {foo=value});
+  local config = getmetatable(prototype);
+  unit.assertIsNil(base.foo);
+  unit.assertEquals(rawget(config.__index, "foo"), value);
+end
+
+function testExtendString()
+  local base = {}; local value = "foo";
+  local prototype = extend(base, {foo=value});
+  local config = getmetatable(prototype);
+  unit.assertIsNil(base.foo);
+  unit.assertEquals(config.instance.foo, value);
+end
+
+function testExtendNumber()
+  local base = {}; local value = 42;
+  local prototype = extend(base, {foo=value});
+  local config = getmetatable(prototype);
+  unit.assertIsNil(base.foo);
+  unit.assertEquals(config.instance.foo, value);
+end
+
+function testExtendBoolean()
+  local base = {}; local value = true;
+  local prototype = extend(base, {foo=value});
+  local config = getmetatable(prototype);
+  unit.assertIsNil(base.foo);
+  unit.assertEquals(config.instance.foo, value);
+end
+
+function testPrototypeAddPrefixedValue()
+  local base = {}; local value = "bar";
+  local prototype = extend(base);
+  prototype.__foo = value;
+  local config = getmetatable(prototype);
+  unit.assertIsNil(base.__foo);
+  unit.assertIsNil(config.instance.__foo);
+  unit.assertIsNil(rawget(config.__index, "__foo"));
+  unit.assertEquals(config.meta.__foo, value);
+end
+
+function testPrototypeAddFunction()
+  local base = {}; local value = function() end;
+  local prototype = extend(base);
+  prototype.foo = value;
+  local config = getmetatable(prototype);
+  unit.assertIsNil(base.foo);
+  unit.assertIsNil(config.instance.foo);
+  unit.assertIsNil(config.meta.foo);
+  unit.assertEquals(rawget(config.__index, "foo"), value);
+end
+
+function testPrototypeAddTable()
+  local base = {}; local value = {};
+  local prototype = extend(base);
+  prototype.foo = value;
+  local config = getmetatable(prototype);
+  unit.assertIsNil(base.foo);
+  unit.assertIsNil(config.instance.foo);
+  unit.assertIsNil(config.meta.foo);
+  unit.assertEquals(rawget(config.__index, "foo"), value);
+end
+
+function testPrototypeAddString()
+  local base = {}; local value = "foo";
+  local prototype = extend(base);
+  prototype.foo = value;
+  local config = getmetatable(prototype);
+  unit.assertIsNil(base.foo);
+  unit.assertIsNil(config.meta.foo);
+  unit.assertIsNil(rawget(config.__index, "foo"));
+  unit.assertEquals(config.instance.foo, value);
+end
+
+function testPrototypeAddNumber()
+  local base = {}; local value = 42;
+  local prototype = extend(base);
+  prototype.foo = value;
+  local config = getmetatable(prototype);
+  unit.assertIsNil(base.foo);
+  unit.assertIsNil(config.meta.foo);
+  unit.assertIsNil(rawget(config.__index, "foo"));
+  unit.assertEquals(config.instance.foo, value);
+end
+
+function testPrototypeAddBoolean()
+  local base = {}; local value = true;
+  local prototype = extend(base);
+  prototype.foo = value;
+  local config = getmetatable(prototype);
+  unit.assertIsNil(base.foo);
+  unit.assertIsNil(config.meta.foo);
+  unit.assertIsNil(rawget(config.__index, "foo"));
+  unit.assertEquals(config.instance.foo, value);
+end
+
+function testPrototypeAlterPrefixedValue()
+  local base = {}; local value = "bar";
+  local prototype = extend(base, {__foo="foo"});
+  prototype.__foo = value;
+  local config = getmetatable(prototype);
+  unit.assertIsNil(base.__foo);
+  unit.assertIsNil(config.instance.__foo);
+  unit.assertIsNil(rawget(config.__index, "__foo"));
+  unit.assertEquals(config.meta.__foo, value);
+end
+
+function testPrototypeAlterStaticToInstance()
+  local base = {}; local value = "bar";
+  local prototype = extend(base, {foo=function() end});
+  prototype.foo = value;
+  local config = getmetatable(prototype);
+  unit.assertIsNil(base.foo);
+  unit.assertIsNil(config.meta.foo);
+  unit.assertIsNil(rawget(config.__index, "foo"));
+  unit.assertEquals(config.instance.foo, value);
+end
+
+function testPrototypeAlterInstanceToStatic()
+  local base = {}; local value = function() end;
+  local prototype = extend(base, {foo=42});
+  prototype.foo = value;
+  local config = getmetatable(prototype);
+  unit.assertIsNil(base.foo);
+  unit.assertIsNil(config.instance.foo);
+  unit.assertIsNil(config.meta.foo);
+  unit.assertEquals(rawget(config.__index, "foo"), value);
+end
+
+function testPrototypeAlterInstanceToInstance()
+  local base = {}; local value = "bar";
+  local prototype = extend(base, {foo=42});
+  prototype.foo = value;
+  local config = getmetatable(prototype);
+  unit.assertIsNil(base.foo);
+  unit.assertIsNil(config.meta.foo);
+  unit.assertIsNil(rawget(config.__index, "foo"));
+  unit.assertEquals(config.instance.foo, value);
+end
+
+function testPrototypeAlterStaticToStatic()
+  local base = {}; local value = function() end;
+  local prototype = extend(base, {foo=function() end});
+  prototype.foo = value;
+  local config = getmetatable(prototype);
+  unit.assertIsNil(base.foo);
+  unit.assertIsNil(config.instance.foo);
+  unit.assertIsNil(config.meta.foo);
+  unit.assertEquals(rawget(config.__index, "foo"), value);
+end
+
+function testPrototypeExposesStatic()
+  local template = {tbl={}, func=function() end, str="foo", num=42, bool=true};
+  local prototype = extend({}, template);
+  unit.assertIs(prototype.tbl, template.tbl);
+  unit.assertEquals(prototype.func, template.func);
+  unit.assertIsNil(prototype.str);
+  unit.assertIsNil(prototype.num);
+  unit.assertIsNil(prototype.bool);
+end
+
+function testCanInitialize() unit.assertEquals(type(extend({})()), "table"); end
+
+function testInstanceInheritsPrototype()
+  local prototype = extend({});
+  local instance = prototype();
+  unit.assertIs(getmetatable(instance).__index, getmetatable(prototype).__index);
+end
+
+function testInstanceInheritsBaseValues()
+  local base = {tbl={}, func=function() end, str="foo", num=42, bool=true};
+  local instance = extend(base)();
+  unit.assertIs(instance.tbl, base.tbl);
+  unit.assertEquals(instance.func, base.func);
+  unit.assertEquals(instance.str, base.str);
+  unit.assertEquals(instance.num, base.num);
+  unit.assertEquals(instance.bool, base.bool);
+  base.tbl={}; base.func=function() end; base.str="bar"; base.num=43; base.bool=false;
+  unit.assertIs(instance.tbl, base.tbl);
+  unit.assertEquals(instance.func, base.func);
+  unit.assertEquals(instance.str, base.str);
+  unit.assertEquals(instance.num, base.num);
+  unit.assertEquals(instance.bool, base.bool);
+end
+
+function testInstanceStaticValues()
+  local template = {tbl={}, func=function() end};
+  local prototype = extend({}, template);
+  unit.assertIs(prototype.tbl, template.tbl);
+  unit.assertEquals(prototype.func, template.func);
+  local instance = prototype();
+  unit.assertIs(instance.tbl, template.tbl);
+  unit.assertEquals(instance.func, template.func);
+  template.tbl={} template.func=function() end;
+  unit.assertNotIs(prototype.tbl, template.tbl);
+  unit.assertNotIs(instance.tbl, template.tbl);
+  prototype.tbl={}; prototype.func=function() end;
+  unit.assertIs(instance.tbl, prototype.tbl);
+  unit.assertEquals(instance.func, prototype.func);
+end
+
+function testInstanceInstanceValues()
+  local template = {str="foo", num=42, bool=true};
+  local prototype = extend({}, template);
+  unit.assertIsNil(prototype.str);
+  unit.assertIsNil(prototype.num);
+  unit.assertIsNil(prototype.bool);
+  unit.assertEquals(template.str, getmetatable(prototype).instance.str);
+  unit.assertEquals(template.num, getmetatable(prototype).instance.num);
+  unit.assertEquals(template.bool, getmetatable(prototype).instance.bool);
+  template.str="bar"; template.num=43; template.bool=false;
+  unit.assertNotEquals(template.str, getmetatable(prototype).instance.str);
+  unit.assertNotEquals(template.num, getmetatable(prototype).instance.num);
+  unit.assertNotEquals(template.bool, getmetatable(prototype).instance.bool);
+  local instance = prototype();
+  unit.assertEquals(instance.str, getmetatable(prototype).instance.str);
+  unit.assertEquals(instance.num, getmetatable(prototype).instance.num);
+  unit.assertEquals(instance.bool, getmetatable(prototype).instance.bool);
+  getmetatable(prototype).instance.str="baz"; getmetatable(prototype).instance.num=44; getmetatable(prototype).instance.bool=false;
+  unit.assertNotEquals(instance.str, getmetatable(prototype).instance.str);
+  unit.assertNotEquals(instance.num, getmetatable(prototype).instance.num);
+  unit.assertNotEquals(instance.bool, getmetatable(prototype).instance.bool);
+  local instance2 = prototype();
+  unit.assertEquals(instance2.str, getmetatable(prototype).instance.str);
+  unit.assertEquals(instance2.num, getmetatable(prototype).instance.num);
+  unit.assertEquals(instance2.bool, getmetatable(prototype).instance.bool);
+end
+
+function testInstanceMetaValues()
+  local template = {__foo="bar"};
+  local prototype = extend({}, template);
+  unit.assertIsNil(prototype.__foo);
+  unit.assertNotEquals(getmetatable(prototype).__foo, template.__foo);
+  unit.assertEquals(getmetatable(prototype).meta.__foo, template.__foo);
+  template.__foo="baz";
+  unit.assertNotEquals(getmetatable(prototype).meta.__foo, template.__foo);
+  local instance = prototype();
+  unit.assertIsNil(instance.__foo);
+  unit.assertEquals(getmetatable(instance).__foo, getmetatable(prototype).meta.__foo);
+  getmetatable(prototype).meta.__foo="qux";
+  unit.assertNotEquals(getmetatable(instance).__foo, getmetatable(prototype).meta.__foo);
+  local instance2 = prototype();
+  unit.assertEquals(getmetatable(instance2).__foo, getmetatable(prototype).meta.__foo);
+end
+
+function testInheritanceMultiple()
+  local base = {baseStr="base", baseNum=42, baseBool=true, baseTbl={}, baseFunc=function() end};
+  local super = {superStr="super", superNum=43, superBool=false, superTbl={}, superFunc=function() end};
+  local sub = {subStr="sub", subNum=44, subBool=true, subTbl={}, subFunc=function() end};
+  local instance = extend(extend(base, super), sub)();
+  unit.assertEquals(instance.baseStr, base.baseStr);
+  unit.assertEquals(instance.baseNum, base.baseNum);
+  unit.assertEquals(instance.baseBool, base.baseBool);
+  unit.assertIs(instance.baseTbl, base.baseTbl);
+  unit.assertEquals(instance.baseFunc, base.baseFunc);
+  unit.assertEquals(instance.superStr, super.superStr);
+  unit.assertEquals(instance.superNum, super.superNum);
+  unit.assertEquals(instance.superBool, super.superBool);
+  unit.assertIs(instance.superTbl, super.superTbl);
+  unit.assertEquals(instance.superFunc, super.superFunc);
+  unit.assertEquals(instance.subStr, sub.subStr);
+  unit.assertEquals(instance.subNum, sub.subNum);
+  unit.assertEquals(instance.subBool, sub.subBool);
+  unit.assertIs(instance.subTbl, sub.subTbl);
+  unit.assertEquals(instance.subFunc, sub.subFunc);
+end
+
+function testInheritanceOverridesFirstToLast()
+  local base = {a=1, b=1, c=1};
+  local super = {b=2, c=2};
+  local sub = {c=3};
+  local instance = extend(extend(base, super), sub)();
+  unit.assertEquals(instance.a, base.a);
+  unit.assertEquals(instance.b, super.b);
+  unit.assertEquals(instance.c, sub.c);
+end
+
+os.exit(unit.LuaUnit.run());
